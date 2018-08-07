67e9b9709911ba610c433158aa57044870cae107
mVang <meng.vang92@yahoo.com>
2018-08-06 20:44:54 -0500

    Modal, friend, survey updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,163 +1,131 @@
 // Data structure that will store the friends data
 var friendsArray = [
 	{
-		name: 'Scooter',
-		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		name: "Pinhead Larry",
+		photo: "https://i.imgur.com/Hc5ZBAU.jpg",
 		scores: [
-			'1',
-			'5',
-			'5',
-			'1',
-			'5',
-			'5',
-			'1',
-			'1',
-			'1',
-			'5'
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
 		]
 	},
 	{
-		name: 'Fluffy',
-		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+		name: "Dirty Dan",
+		photo: "https://vignette.wikia.nocookie.net/spongebobgalaxy/images/3/35/029a_-_Survival_of_the_Idiots_348.jpg/revision/latest?cb=20140805180731",
 		scores: [
-			'2',
-			'4',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'2',
-			'1',
-			'5'
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
 		]
 	},
 	{
-		name: 'Spencer',
-		photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+		name: "Squidward",
+		photo: "https://pbs.twimg.com/profile_images/479419169134231553/35C2XxQY_400x400.png",
 		scores: [
-			'4',
-			'3',
-			'5',
-			'1',
-			'1',
-			'5',
-			'5',
-			'1',
-			'1',
-			'1'
+			"4",
+			"3",
+			"5",
+			"1",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1"
 		]
 	},
 	{
-		name: 'Zackariah',
-		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		name: "Dwight",
+		photo: "https://lindagodfrey.files.wordpress.com/2009/12/dwight.jpg",
 		scores: [
-			'5',
-			'1',
-			'2',
-			'1',
-			'3',
-			'4',
-			'1',
-			'3',
-			'3',
-			'4'
+			"5",
+			"1",
+			"2",
+			"1",
+			"3",
+			"4",
+			"1",
+			"3",
+			"3",
+			"4"
 		]
 	},
 	{
-		name: 'Cloe',
-		photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+		name: "Michael",
+		photo: "https://boston.citymomsblog.com/wp-content/uploads/sites/55/2018/02/does-the-office-hold-up-gq.jpg",
 		scores: [
-			'1',
-			'1',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'1'
+			"1",
+			"1",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"1"
 		]
 	},
 	{
-		name: 'Mrs. Piggy',
-		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		name: "Homer",
+		photo: "https://nerdist.com/wp-content/uploads/2018/03/Homer-Simpson-drooling.jpg",
 		scores: [
-			'1',
-			'1',
-			'5',
-			'1',
-			'3',
-			'2',
-			'5',
-			'1',
-			'1',
-			'5'
+			"1",
+			"1",
+			"5",
+			"1",
+			"3",
+			"2",
+			"5",
+			"1",
+			"1",
+			"5"
 		]
 	},
 	{
-		name: 'Beyonce Knowles',
-		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		name: "Bart",
+		photo: "https://vignette.wikia.nocookie.net/villains/images/3/39/Bart-simpson.gif/revision/latest?cb=20120114223254",
 		scores: [
-			'5',
-			'4',
-			'4',
-			'2',
-			'4',
-			'4',
-			'3',
-			'2',
-			'3',
-			'3'
+			"5",
+			"4",
+			"4",
+			"2",
+			"4",
+			"4",
+			"3",
+			"2",
+			"3",
+			"3"
 		]
 	},
 	{
-		name: 'Adele Adkins',
-		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		name: "Peter",
+		photo: "https://pbs.twimg.com/profile_images/637788232164372480/5LBqC0Bb_400x400.png",
 		scores: [
-			'5',
-			'2',
-			'3',
-			'1',
-			'5',
-			'1',
-			'3',
-			'1',
-			'1',
-			'5'
-		]
-	},
-	{
-		name: 'Kermit the Frog',
-		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
-		scores: [
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5'
-		]
-	},
-	{
-		name: 'Elmo',
-		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
-		scores: [
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1'
+			"5",
+			"2",
+			"3",
+			"1",
+			"5",
+			"1",
+			"3",
+			"1",
+			"1",
+			"5"
 		]
 	}
 ];
