b4c1ccdf3a9586f6c092ccf70b4774f7bb03a4d2
unknown <mVang92@github.com>
2018-08-07 23:31:48 -0500

    CSS updates on background, jumbotron, others

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,15 @@ var friendsArray = [
 		name: "Pinhead Larry",
 		photo: "https://i.imgur.com/Hc5ZBAU.jpg",
 		scores: [
+			"1",
+			"4",
 			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"5",
+			"1",
+			"4",
+			"2",
+			"1",
+			"3",
+			"1",
 			"5"
 		]
 	},
@@ -20,80 +20,80 @@ var friendsArray = [
 		name: "Dirty Dan",
 		photo: "https://vignette.wikia.nocookie.net/spongebobgalaxy/images/3/35/029a_-_Survival_of_the_Idiots_348.jpg/revision/latest?cb=20140805180731",
 		scores: [
-			"1",
-			"1",
-			"1",
-			"1",
-			"1",
-			"1",
-			"1",
-			"1",
-			"1",
-			"1"
+			"3",
+			"4",
+			"3",
+			"2",
+			"3",
+			"4",
+			"3",
+			"2",
+			"2",
+			"3"
 		]
 	},
 	{
 		name: "Squidward",
 		photo: "https://pbs.twimg.com/profile_images/479419169134231553/35C2XxQY_400x400.png",
 		scores: [
-			"4",
+			"1",
 			"3",
+			"3",
+			"1",
+			"2",
 			"5",
+			"2",
 			"1",
 			"1",
-			"5",
-			"5",
-			"1",
-			"1",
-			"1"
+			"5"
 		]
 	},
 	{
 		name: "Dwight",
 		photo: "https://lindagodfrey.files.wordpress.com/2009/12/dwight.jpg",
 		scores: [
-			"5",
-			"1",
+			"3",
 			"2",
-			"1",
 			"3",
+			"2",
 			"4",
-			"1",
+			"4",
 			"3",
 			"3",
-			"4"
+			"3",
+			"3"
 		]
 	},
 	{
 		name: "Michael",
 		photo: "https://boston.citymomsblog.com/wp-content/uploads/sites/55/2018/02/does-the-office-hold-up-gq.jpg",
 		scores: [
-			"1",
-			"1",
+			"4",
+			"4",
+			"3",
+			"2",
 			"5",
+			"4",
+			"3",
 			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"5",
-			"1"
+			"3",
+			"4"
 		]
 	},
 	{
 		name: "Homer",
 		photo: "https://nerdist.com/wp-content/uploads/2018/03/Homer-Simpson-drooling.jpg",
 		scores: [
-			"1",
-			"1",
 			"5",
-			"1",
+			"4",
+			"3",
+			"4",
 			"3",
 			"2",
 			"5",
-			"1",
-			"1",
-			"5"
+			"4",
+			"2",
+			"2"
 		]
 	},
 	{
